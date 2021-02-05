6686c9e64f57179d835a422e73830675f543488d
RHolmgaa <68645481+RHolmgaa@users.noreply.github.com>
2021-02-05 16:13:55 +0100

    Update Audit Folder.ps1
    Script got A LOT of changes to make it fater in case you're getting permissions from 50k+ folders
    Now it caches the users it finds, so it doesn't have to ask AD for information 100k times

--- Audit Folder.ps1
+++ Audit Folder.ps1
@@ -1,49 +1,62 @@
-#Written by Rasmus Holmgaard
 Write-Host "Paste in the root folder" -ForegroundColor Yellow
 $RootFolder = Read-Host
 Write-Host "Where should the audit export" -ForegroundColor Yellow
 $Export = Read-Host
 
-#First get the list of users/groups that have access to the main folder
-$RootAcl = Get-Acl $RootFolder
+$Report = @()
+$Cache = @()
 
-#Create a report for each User/Group - root folder
-foreach ($RootAccessObject in $RootAcl.Access) {
-    [string]$IdentityReference = ($AccessObject.IdentityReference)
-    Write-Host $RootFolder
+#First collect all the folders that we need to get permissions from
+$FolderPath = Get-ChildItem -Directory -Path $RootFolder -Recurse -Force
+Write-Host "Folders Collected"
+foreach ($Folder in $FolderPath) {
 
-    #Report for each IdentityReference
-    $Report = New-Object PSObject
+    #Check each for permissions
+    $Acl = Get-Acl -Path $Folder.FullName
+    foreach ($Access in $Acl.Access) {
 
-    $Report | Add-Member -MemberType NoteProperty -Name Domain -Value $IdentityReference.Split('\')[0]
-    $Report | Add-Member -MemberType NoteProperty -Name User -Value $IdentityReference.Split('\')[1]
-    $Report | Add-Member -MemberType NoteProperty -Name IsInherited -Value $RootAccessObject.IsInherited
-    $Report | Add-Member -MemberType NoteProperty -Name InheritanceFlags -Value $RootAccessObject.InheritanceFlags
-    $Report | Add-Member -MemberType NoteProperty -Name FileSystemRights -Value $RootAccessObject.FileSystemRights
-    $Report | Add-Member -MemberType NoteProperty -Name Path -Value $RootFolder
+        #I dont't want to see entries from BUILTIN\Administrators, NT AUTHORITY\SYSTEM, CREATOR OWNER, BUILTIN\Users
+        if (!($Access.IdentityReference -eq "BUILTIN\Administrators") -and !($Access.IdentityReference -eq "NT AUTHORITY\SYSTEM") -and !($Access.IdentityReference -eq "CREATOR OWNER") -and !($Access.IdentityReference -eq "BUILTIN\Users")) {
+            $User = $null
 
-    [string]$Path = $Export + "\$($Report.User).csv"
-    $Report | Export-Csv -Path $Path -Force -Append -NoTypeInformation
-}
-
-#Create a report for each User/Group - subfolder
-Get-ChildItem -Path $RootFolder -Directory -Recurse -Force | select FullName | ForEach-Object {
-    Write-Host $_.FullName
-    $SubAcl = Get-Acl $_.FullName
-    foreach ($SubAccessObject in $SubAcl.Access) {
-        [string]$IdentityReference = ($SubAccessObject.IdentityReference)
-
-        #Report for each IdentityReference
-        $Report = New-Object PSObject
-    
-        $Report | Add-Member -MemberType NoteProperty -Name Domain -Value $IdentityReference.Split('\')[0]
-        $Report | Add-Member -MemberType NoteProperty -Name User -Value $IdentityReference.Split('\')[1]
-        $Report | Add-Member -MemberType NoteProperty -Name IsInherited -Value $SubAccessObject.IsInherited
-        $Report | Add-Member -MemberType NoteProperty -Name InheritanceFlags -Value $SubAccessObject.InheritanceFlags
-        $Report | Add-Member -MemberType NoteProperty -Name FileSystemRights -Value $SubAccessObject.FileSystemRights
-        $Report | Add-Member -MemberType NoteProperty -Name Path -Value $_.FullName
-
-        [string]$Path = $Export + "\$($Report.User).csv"
-        $Report | Export-Csv -Path $Path -Force -Append -NoTypeInformation
+            #We want to cache the users for better performance and to avoid ased AD for users 100k times
+            if (!($Cache.SID -contains $Access.IdentityReference)) {
+                try {
+                    $User = Get-ADUser -Identity "$($Access.IdentityReference)"
+                    Write-Host $User.Name -ForegroundColor Green
+                    $Cache += [PSCustomObject]@{
+                        SID  = $User.SID
+                        Name = $User.Name
+                    }
+                }
+                catch {
+                    $Cache += [PSCustomObject]@{
+                        SID  = $Access.IdentityReference
+                        Name = $null
+                    }
+                    #This will only fail if it gets an SID from a user that is deleted.
+                    Write-Host "Failed" $Access.IdentityReference
+                }
+            }
+            
+            #Make it so the users can only be pulled from the cache when put into the report.
+            elseif ($Cache.SID -contains $Access.IdentityReference) {
+                Write-Host $Access.IdentityReference "Found in cache"
+                $Properties = [ordered]@{
+                    'FolderName'        = $RootFolder
+                    'IdentityReference' = $Cache.where{ ($_.SID -eq $Access.IdentityReference) }.SID
+                    'AD Group or User'  = $Cache.where{ ($_.Name -eq $Access.IdentityReference) }.Name
+                    'Permissions'       = $Access.FileSystemRights
+                }
+            }
+            else {
+                #Just in case the user doesn't get pulled from the cache we want to know, mistakes happen
+                Write-Host "ESCAPED CACHE" $Access.IdentityReference -ForegroundColor Red
+            }
+            $Report += New-Object -TypeName PSObject -Property $Properties
+        }
     }
+    Write-host $Folder.FullName "Done" -ForegroundColor Green
 }
+$Path = $Export + "\Report.csv"
+$Report | Export-Csv -path $Path -Force -Append -NoTypeInformation
